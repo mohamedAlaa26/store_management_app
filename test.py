3ee6044e537f7adcc22ae04782fbf0e86a03e2de
Mohamed Alaa <119450477+mohamedAlaa26@users.noreply.github.com>
2025-03-08 17:33:59 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from models import Base
 
-# Update this with your actual PostgreSQL database URL
+
 DATABASE_URL = 'postgresql://postgres:1234@localhost/Store_DB'
 
 engine = create_engine(DATABASE_URL)
